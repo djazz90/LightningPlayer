92aa17d73a26e15d64ac8887b8467d589f03dae3
David Pinter <djazz90@gmail.com>
2017-04-18 20:51:14 +0200

    #25 Properties initialization is better. API keys are loaded from properties file.

--- src/main/java/hu/davidp/beadando/player/model/PlayerSettings.java
+++ src/main/java/hu/davidp/beadando/player/model/PlayerSettings.java
@@ -5,6 +5,7 @@ import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Properties;
@@ -15,9 +16,11 @@ public final class PlayerSettings {
     private static final File PROPERTIES_FILE = new File("settings.properties");
 
     private static final Double DEFAULT_VOLUME_LEVEL = 0.7;
+    private static final String DEFAULT_API_KEY = "none";
 
     private static final String VOLUME_LEVEL_PROPERTY_NAME = "volume.level";
     private static final String NAVIGATION_STATE_PROPERTY_NAME = "navigation.state";
+    private static final String API_KEY_PROPERTY_NAME = "api.key";
 
     @Getter
     @Setter
@@ -27,26 +30,56 @@ public final class PlayerSettings {
     @Setter
     private static NavigationState navigationState = NavigationState.NEXT_SONG;
 
+    @Getter
+    @Setter
+    private static String apiKey = DEFAULT_API_KEY;
+
     public enum NavigationState {
         NEXT_SONG, REPEAT_SONG, REPEAT_PLAYLIST, SHUFFLE
     }
 
     public static void initialize() {
         if (!PROPERTIES_FILE.exists()) {
-            properties.setProperty(VOLUME_LEVEL_PROPERTY_NAME, volumeLevel.toString());
-            properties.setProperty(NAVIGATION_STATE_PROPERTY_NAME, navigationState.toString());
+            save();
+        } else {
+            load();
+        }
+    }
 
-            try (FileOutputStream outputStream = new FileOutputStream(PROPERTIES_FILE)) {
-                properties.store(outputStream, null);
-                log.info("The initial property file is created!");
-            } catch (IOException e) {
-                log.error("The initial property file cannot be created!", e);
-            }
+    public static void load() {
+        try (FileInputStream inputStream = new FileInputStream(PROPERTIES_FILE)) {
+            properties.load(inputStream);
+            log.info("The property file is successfully loaded!");
+
+            volumeLevel = Double.valueOf(properties.getProperty(VOLUME_LEVEL_PROPERTY_NAME));
+            navigationState = NavigationState.valueOf(properties.getProperty(NAVIGATION_STATE_PROPERTY_NAME));
+            apiKey = properties.getProperty(API_KEY_PROPERTY_NAME);
+            log.info("api key: {}", apiKey);
+
+        } catch (IOException e) {
+            log.error("The property file cannot be loaded!", e);
+        }
+
+
+    }
+
+    public static void save() {
+        properties.setProperty(VOLUME_LEVEL_PROPERTY_NAME, volumeLevel.toString());
+        properties.setProperty(NAVIGATION_STATE_PROPERTY_NAME, navigationState.toString());
+        properties.setProperty(API_KEY_PROPERTY_NAME, apiKey);
+
+        try (FileOutputStream outputStream = new FileOutputStream(PROPERTIES_FILE)) {
+            properties.store(outputStream, null);
+            log.info("The initial property file is created!");
+        } catch (IOException e) {
+            log.error("The initial property file cannot be created!", e);
         }
     }
 
     private PlayerSettings() {
         //hidden utility class constructor
     }
+
+
 
 }
